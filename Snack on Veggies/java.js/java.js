79d2aff8300233c7e172c223ff1bcfd8bda96f2e
kmoagi <33934323+kmoagi@users.noreply.github.com>
2020-01-29 06:50:57 +0200

    Add files via upload

---
+++ Snack on Veggies/java.js/java.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+function openNav() {
+  document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+}
+
+function myFunction() {
+  setTimeout(function () {
+    var benana = document.createElement("img");
+    alert("Hello");
+
+  }, 3000);
+}
